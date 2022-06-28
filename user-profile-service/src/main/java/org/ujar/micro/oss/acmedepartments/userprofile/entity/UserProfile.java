b0c96b662879fc8fd21579d0437dfb7398c5a367
Dima Denysenko <1253839+dimdnk@users.noreply.github.com>
2022-06-28 14:26:45 +0300

    feat: create user profile service, which uses mongodb to store data (#1)

---
+++ user-profile-service/src/main/java/org/ujar/micro/oss/acmedepartments/userprofile/entity/UserProfile.java
@@ -0,0 +1,38 @@
+package org.ujar.micro.oss.acmedepartments.userprofile.entity;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = UserProfile.COLLECTION_NAME)
+public class UserProfile {
+
+  protected static final String COLLECTION_NAME =  "user_profiles";
+
+  @Id
+  private String id;
+
+  @Email
+  @Size(min = 5, max = 254)
+  private String email;
+
+  private boolean active;
+
+  @Override
+  public String toString() {
+    return "UserProfile{" +
+           "id=" + id +
+           ", email='" + email + '\'' +
+           ", active=" + active +
+           '}';
+  }
+}
