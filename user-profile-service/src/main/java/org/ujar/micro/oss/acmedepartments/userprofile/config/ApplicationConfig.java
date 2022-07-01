1ea73d6416466f0f399c407665b20889dfa22274
Dima Denysenko <1253839+dimdnk@users.noreply.github.com>
2022-07-01 11:07:32 +0300

    feat: enable config-server, hystrix-dashboard, sleuth and zipkin (#6)

--- user-profile-service/src/main/java/org/ujar/micro/oss/acmedepartments/userprofile/config/ApplicationConfig.java
+++ user-profile-service/src/main/java/org/ujar/micro/oss/acmedepartments/userprofile/config/ApplicationConfig.java
@@ -1,10 +1,8 @@
 package org.ujar.micro.oss.acmedepartments.userprofile.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import org.ujar.boot.starter.logbook.LogbookJsonBodyFilter;
 import org.ujar.boot.starter.logbook.LogbookResponseOnStatus;
 import org.ujar.micro.oss.acmedepartments.userprofile.client.DepartmentServiceClient;
-import org.ujar.micro.oss.acmedepartments.userprofile.client.impl.DepartmentServiceClientImpl;
+import org.ujar.micro.oss.acmedepartments.userprofile.client.DepartmentServiceClientImpl;
 import org.ujar.micro.oss.acmedepartments.userprofile.config.properties.RestfulApiResourcesProperties;
 import org.ujar.micro.oss.acmedepartments.userprofile.config.properties.RestfulClientProperties;
 
@@ -24,12 +22,6 @@ import org.ujar.micro.oss.acmedepartments.userprofile.config.properties.RestfulC
 @EnableEurekaClient
 @EnableConfigurationProperties({RestfulClientProperties.class, RestfulApiResourcesProperties.class})
 public class ApplicationConfig {
-
-  @Bean
-  @LoadBalanced
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
 
   @Bean
   DepartmentServiceClient departmentServiceClient(
--- user-profile-service/src/main/java/org/ujar/micro/oss/acmedepartments/userprofile/dto/UserProfileWithDepartmentDto.java
+++ user-profile-service/src/main/java/org/ujar/micro/oss/acmedepartments/userprofile/dto/UserProfileWithDepartmentDto.java
@@ -1,7 +1,9 @@
 package org.ujar.micro.oss.acmedepartments.userprofile.dto;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.ujar.micro.oss.acmedepartments.userprofile.client.dto.DepartmentResponse;
 
+@SuppressFBWarnings("EI_EXPOSE_REP")
 public record UserProfileWithDepartmentDto(String email, String firstName, String lastName, Long departmentId,
                                            DepartmentResponse department) {
 
