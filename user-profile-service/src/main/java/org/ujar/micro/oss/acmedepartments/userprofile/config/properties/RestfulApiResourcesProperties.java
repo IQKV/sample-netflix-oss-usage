67aee326709c4d824800cf768e7e4684a502d375
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2022-08-08 19:47:22 +0300

    feat: use records for props
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- user-profile-service/src/main/java/org/ujar/micro/oss/acmedepartments/userprofile/config/properties/RestfulApiResourcesProperties.java
+++ user-profile-service/src/main/java/org/ujar/micro/oss/acmedepartments/userprofile/config/properties/RestfulApiResourcesProperties.java
@@ -1,13 +1,20 @@
 package org.ujar.micro.oss.acmedepartments.userprofile.config.properties;
 
-import lombok.Value;
+import lombok.NonNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
-@Value
 @ConstructorBinding
 @ConfigurationProperties(prefix = "ujar.restful-api-resources")
-public class RestfulApiResourcesProperties {
-  String departmentServiceBaseUrl;
-  String departmentServiceUri;
+public record RestfulApiResourcesProperties(@NonNull String departmentServiceBaseUrl,
+                                            @NonNull String departmentServiceUri) {
+
+
+  public String getDepartmentServiceBaseUrl() {
+    return departmentServiceBaseUrl();
+  }
+
+  public String getDepartmentServiceUri() {
+    return departmentServiceUri();
+  }
 }
