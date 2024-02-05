1de2e3e2dd4836c2b69cbce7999e5f27e2f20c92
Dima Denysenko <coding@dimdnk.com>
2024-02-05 14:20:33 +0200

    fix: unness

--- user-profile-service/src/test/java/dev/knowhowto/acmecloudnetflix/userprofile/UserProfileServiceApplicationIT.java
+++ user-profile-service/src/test/java/dev/knowhowto/acmecloudnetflix/userprofile/UserProfileServiceApplicationIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SuppressWarnings({"PMD.ClassNamingConventions", "PMD.UnusedPrivateMethod"})
 class UserProfileServiceApplicationIT {
 
   @Test
